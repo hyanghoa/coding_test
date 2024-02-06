98515dc763d6bbc32be68def432756bd625f0fab
Dongjun Lee <34564962+hyanghoa@users.noreply.github.com>
2024-02-06 18:26:26 +0900

    feat: 알고리즘 풀이 추가

--- 배열/11 자신을 제외한 배열의 곱.py
+++ 배열/11 자신을 제외한 배열의 곱.py
@@ -3,4 +3,15 @@
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
+        answer = []
+        left_list = [1]
+        right_list = [1]
+
+        for i in range(len(nums)):
+            left_list.append(left_list[-1] * nums[i])
+            right_list.insert(0, right_list[0] * nums[-i - 1])
+
+        for x, y in zip(left_list[:-1], right_list[1:]):
+            answer.append(x * y)
+
+        return answer
